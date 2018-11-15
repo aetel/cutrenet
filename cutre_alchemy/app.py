f3af5a93890018a619f6a24cda34a2530378b962
Ricardo <me@ricardosa.nz>
2018-11-15 01:21:57 +0100

    User edit now works

--- cutre_alchemy/app.py
+++ cutre_alchemy/app.py
@@ -38,55 +38,87 @@ def create_user():
 
 # Views
 @app.route('/')
+def home():
+    return 'Hello'
+
+@app.route('/members')
 @login_required
 def member_database():
     results = db_session.query(User).all()
-    print(results)
+    db_session.commit()
     return render_template('database.html', results=results)
 
 # this is when user clicks edit link
-@app.route('/edit', methods=['GET'])
+@app.route('/members/edit', methods=['GET'])
 @login_required
 def edit_member():
     eresult = None
     if request.method == 'GET':
         edni = request.args.get('edni')
         eresult = db_session.query(User).filter_by(dni=edni).first()
-    results = results = db_session.query(User).all()
+    results = db_session.query(User).all()
+    db_session.commit()
     return render_template('database.html', eresult=eresult, results=results)
 
-@app.route('/edit', methods=['POST'])
+@app.route('/members/edit', methods=['POST'])
 @login_required
 def sql_dataedit():
     #eresult["first_name"] eresult["last_name"]
     if request.method == 'POST':
+        # old_dni = request.form['old_dni']
+        # last_name = request.form['last_name']
+        # first_name = request.form['first_name']
+        # email = request.form['email']
+        # dni = request.form['dni']
+        # school = request.form['school']
+        # degree = request.form['degree']
+        # year = request.form['year']
+        # telegram = request.form['telegram']
+        # password = request.form['password']
+        # user_datastore.create_user(email=email, password=password, dni=dni, first_name=first_name, last_name=last_name, school=school, degree=degree, year=year, telegram=telegram)
+        # values = email=email, password=password, dni=dni, first_name=first_name, last_name=last_name, school=school, degree=degree, year=year, telegram=telegram]
+        #db_session.user.update().where(users.c.id==5).values(email=email, password=password, dni=dni, first_name=first_name, last_name=last_name, school=school, degree=degree, year=year, telegram=telegram)
         old_dni = request.form['old_dni']
-        last_name = request.form['last_name']
-        first_name = request.form['first_name']
-        email = request.form['email']
-        dni = request.form['dni']
-        school = request.form['school']
-        degree = request.form['degree']
-        year = request.form['year']
-        telegram = request.form['telegram']
-        password = request.form['password']
-        user_datastore.create_user(email=email, password=password, dni=dni, first_name=first_name, last_name=last_name, school=school, degree=degree, year=year, telegram=telegram)
-        db_session.commit()
+
+        user = db_session.query(User).filter_by(dni=old_dni).first()
+        user.last_name = request.form['last_name']
+        user.first_name = request.form['first_name']
+        user.email = request.form['email']
+        user.dni = request.form['dni']
+        user.school = request.form['school']
+        user.degree = request.form['degree']
+        user.year = request.form['year']
+        user.telegram = request.form['telegram']
+        user.password = request.form['password']
+        #update_user(old_dni,email=email, password=password, dni=dni, first_name=first_name, last_name=last_name, school=school, degree=degree, year=year, telegram=telegram)
         flash(u'Editado satisfactoriamente', 'success')
-    results = results = db_session.query(User).all()
-    return redirect('/', code=302)
+    results = db_session.query(User).all()
+    db_session.commit()
+    return redirect('/members', code=302)
+
+# this is when Treasurer clicks activate link
+@app.route('/members/confirm', methods=['POST', 'GET'])
+@login_required
+def confirm_member():
+    if request.method == 'GET':
+        dni = request.args.get('dni')
+        user = db_session.query(User).filter_by(dni=dni).first()
+        user.active = not user.active
+        flash(u'Confirmado satisfactoriamente', 'success')
+    results = db_session.query(User).all()
+    db_session.commit()
+    return render_template('database.html', results=results)
 
 # this is when user clicks delete link
-@app.route('/delete', methods=['POST', 'GET'])
+@app.route('/members/delete', methods=['POST', 'GET'])
 @login_required
 def delete_member():
     if request.method == 'GET':
         dni = request.args.get('dni')
-        user = db_session.query(User).filter_by(dni=dni).first()
-        user_datastore.delete_user(user)
-        db_session.commit()
+        db_session.query.filter(User.id == 123).delete()
         flash(u'Borrado satisfactoriamente', 'success')
-    results = results = db_session.query(User).all()
+    results = db_session.query(User).all()
+    db_session.commit()
     return render_template('database.html', results=results)
 
 
