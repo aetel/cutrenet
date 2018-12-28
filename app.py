858f40f10c699346e48cb104ea44e9a2ca64fcdc
Ricardo <me@ricardosa.nz>
2018-12-28 04:41:49 +0100

    JQuery now only loads when needed and loads at bottom of page

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 465
 app.config['MAIL_USE_SSL'] = True
 app.config['MAIL_USERNAME'] = 'aetel.backend@gmail.com'
-app.config['MAIL_PASSWORD'] = 'backtheend'
+app.config['MAIL_PASSWORD'] = 'password'
 mail = Mail(app)
 
 # File Upload
