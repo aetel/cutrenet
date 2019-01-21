3771090dd7cfc5f51ec04c67fd3ac52976d37782
Ricardo <me@ricardosa.nz>
2019-01-21 18:54:53 +0100

    Fixed indentation

--- functions/email.py
+++ functions/email.py
@@ -4,26 +4,26 @@ import os
 
 
 def email_all(app,mail,data,attachment):
-	users = User.query.filter(User.roles.any(Role.id.in_([2]))).all() # We pick the users that have role member (ID 2 in table roles)
-	with mail.connect() as conn:
-	    for user in users:
-	        msg = Message(sender=("AETEL cutrenet", app.config['MAIL_USERNAME']),
-	        			  recipients=[user.email],
-	                      body=data['message'],
-	                      subject=data['subject'])
-	        if attachment:
-	        	with app.open_resource(os.path.join(app.config['UPLOAD_FOLDER'],attachment)) as fp:
-  					msg.attach(attachment, "image/png", fp.read())
-	        conn.send(msg)
+    users = User.query.filter(User.roles.any(Role.id.in_([2]))).all() # We pick the users that have role member (ID 2 in table roles)
+    with mail.connect() as conn:
+        for user in users:
+            msg = Message(sender=("AETEL cutrenet", app.config['MAIL_USERNAME']),
+                          recipients=[user.email],
+                          body=data['message'],
+                          subject=data['subject'])
+            if attachment:
+                with app.open_resource(os.path.join(app.config['UPLOAD_FOLDER'],attachment)) as fp:
+                    msg.attach(attachment, "image/png", fp.read())
+            conn.send(msg)
 
 def email(app,mail,data,attachment, users):
-	with mail.connect() as conn:
-	    for user in users:
-	        msg = Message(sender=("AETEL cutrenet", app.config['MAIL_USERNAME']),
-	        			  recipients=[user.email],
-	                      body=data['message'],
-	                      subject=data['subject'])
-	        if attachment:
-	        	with app.open_resource(os.path.join(app.config['UPLOAD_FOLDER'],attachment)) as fp:
-  					msg.attach(attachment, "image/png", fp.read())
-	        conn.send(msg)
+    with mail.connect() as conn:
+        for user in users:
+            msg = Message(sender=("AETEL cutrenet", app.config['MAIL_USERNAME']),
+                          recipients=[user.email],
+                          body=data['message'],
+                          subject=data['subject'])
+            if attachment:
+                with app.open_resource(os.path.join(app.config['UPLOAD_FOLDER'],attachment)) as fp:
+                    msg.attach(attachment, "image/png", fp.read())
+            conn.send(msg)
